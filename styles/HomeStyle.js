9fcdab236fc415c1508ae5f0bedc692c373dca82
nabeeltahirdeveloper <nabeeltahirdeveloper@gmail.com>
2022-12-09 23:54:43 +0500

    v1

---
+++ styles/HomeStyle.js
@@ -0,0 +1,19 @@
+const styles = {
+    header:{
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '100%',
+    },
+    headerContainer:{
+        display: 'flex',
+    },
+    headerText:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#000',
+        display: 'flex',
+        gap: 10,
+    }
+}
+
+export default styles;
