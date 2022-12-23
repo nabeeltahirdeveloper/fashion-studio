54beba6973fa419f6c6c17e544e3a512d03dab9b
nabeeltahirdeveloper <nabeeltahirdeveloper@gmail.com>
2022-12-23 09:02:21 +0500

    header fix

--- styles/HomeStyle.js
+++ styles/HomeStyle.js
@@ -3,9 +3,11 @@ const styles = {
         display: 'flex',
         justifyContent: 'space-between',
         width: '100%',
+        alignItems: 'center',
     },
     headerContainer:{
         display: 'flex',
+        alignItems: 'center',
     },
     headerText:{
         fontSize: 20,
@@ -13,6 +15,27 @@ const styles = {
         color: '#000',
         display: 'flex',
         gap: 10,
+        paddingLeft:25
+    },
+    searchField:{
+        marginLeft:36,
+        width:576,
+        height:46,
+        borderRadius: "10px 0px 0px 10px",
+        backgroundColor:"#D9D9D9",
+        border: "none",
+        paddingLeft: 9,
+        fontSize: 20,
+    },
+    searchButton:{
+        width: 59,
+        height: 46,
+        borderRadius: "0px 10px 10px 0px",
+        backgroundColor: '#FF881A',
+        border: "none",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
     }
 }
 
