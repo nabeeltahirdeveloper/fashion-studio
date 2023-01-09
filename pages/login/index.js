b9b8a260a2f3d2c779ca3fbb36aab0cce868eebd
nabeeltahirdeveloper <nabeeltahirdeveloper@gmail.com>
2023-01-09 08:41:15 +0500

    login complete

---
+++ pages/login/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+import Footer from '../../components/footer/Footer'
+import Image from 'next/image'
+import loginImage from "../../assets/images/loginView.png"
+import localStyle from "./loginStyle"
+export default function index() {
+    return (
+        <div>
+            <Header />
+
+            <div className="mainBody" style={localStyle.container}>
+                <div className="leftContainer" style={localStyle.leftContainer}>
+                    <Image
+                        src={loginImage}
+                        alt="Picture of the author"
+                        width={586}
+                        height={572}
+                    />
+                </div>
+                <div className="rightContainer" style={localStyle.rightContainer}>
+                    <div className="loginContainer" style={localStyle.rightMainContainer}>
+                        <h1 style={localStyle.heading}>Log in</h1>
+                        <div style={{...localStyle.inputContainer}}>
+                            <label style={localStyle.inputHeading} htmlFor="email">Email</label>
+                            <input style={localStyle.input} type="email" placeholder='Enter your email' name="email" id="email" />
+                        </div>
+                        <div style={{...localStyle.inputContainer}}>
+                            <label style={localStyle.inputHeading} htmlFor="password">Password</label>
+                            <input style={localStyle.input} type="password" placeholder='Enter your password' name="password" id="password" />
+                        </div>
+                        <div style={localStyle.newUser}>
+                            <p>New user? Create account?</p>
+                        </div>
+                        <div style={localStyle.buttonContainer}>
+                            <button style={localStyle.registerButton}>Register</button>
+                            <button style={localStyle.loginButton}>Log in</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Footer />
+        </div>
+    )
+}
---
+++ pages/login/loginStyle.js
@@ -0,0 +1,86 @@
+import colors from "../../components/colors";
+
+const style = {
+    container: {
+        display: 'flex',
+        width:'100%',
+        justifyContent:'center',
+        height:'60vh'
+    },
+    leftContainer:{
+        display: 'flex',
+        justifyContent: 'center',
+        flex:1
+    },
+    rightContainer:{
+        display: 'flex',
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+
+    },
+    heading:{
+        color:colors.orange,
+        marginBottom:7
+    },
+    rightMainContainer:{
+        display: 'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    inputContainer:{
+        display: 'flex',
+        flexDirection:'column',
+        marginBottom:20
+        
+    },
+    input:{
+        width:550,
+        height:50,
+        backgroundColor:colors.white,
+        border:'none',
+        boxShadow: `1px 1px 10px ${colors.gray}`,
+        paddingTop:12,
+        paddingBottom:12,
+        paddingLeft:8
+    },
+    inputHeading:{
+        fontSize:20,
+        fontWeight:'700',
+        marginBottom:15
+    },
+    newUser:{
+        width:'100%',
+        color:colors.orange,
+        fontSize:20,
+        fontWeight:700
+    },
+    buttonContainer:{
+        display:'flex',
+        justifyContent:'space-between',
+        width:'100%'
+    },
+    registerButton:{
+        backgroundColor:colors.white,
+        border:'none',
+        boxShadow: `1px 1px 10px ${colors.gray}`,
+        width:200,
+        height:50,
+        color:colors.orange,
+        fontSize:20,
+        fontWeight:700
+    },
+    loginButton:{
+        backgroundColor:colors.orange,
+        border:'none',
+        boxShadow: `1px 1px 10px ${colors.gray}`,
+        width:200,
+        height:50,
+        color:colors.white,
+        fontSize:20,
+        fontWeight:700
+    },
+}
+
+export default style;
